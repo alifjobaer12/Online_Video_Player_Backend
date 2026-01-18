96526846563001f6ecf6e15da96e6acb65258d8f
alifjobaer12 <alifjobaer12@gmail.com>
2026-01-18 15:47:40 +0600

    add cors

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-app.use(cors());
+
+/* ✅ CORS CONFIG */
+const allowedOrigins = [
+  "https://online-video-player-frontend.vercel.app",
+  "http://localhost:5173"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET"],
+  })
+);
 
 /* FIX __dirname FOR ES MODULE */
 const __filename = fileURLToPath(import.meta.url);
